c56f47793d2640bd7b709330a6728223746bad24
unknown <S11124144 @student.usp.ac.fj>
2019-10-17 18:37:13 +1200

    Smart Reporting (A little bit)

--- pages/management/js/smart-reporting.js
+++ pages/management/js/smart-reporting.js
@@ -3,11 +3,9 @@
 Function to initailize Pert Diagram
 
 */////////////////////////////////////
-anychart.onDocumentReady(function () {
+$(document).ready(function () {
     //Urls
     var urlGetAllTasks = urlMain + 'api/GetTasksPerProject/'+sessionStorage.getItem('ProjectID');
-    var data;
-    var pert_array = new Array();
     var result = JSON.parse(sessionStorage.getItem('ProjectDetails'));
 
     //Fetch all project tasks
@@ -21,145 +19,101 @@ anychart.onDocumentReady(function () {
         }
     }).then(function (a) { return a.json() })
         .then(function (j) {
+        var array =[];
 
-            for (var i = 0; i < j.length; i++) {
-                if (j[i].PredecessorTaskID == 0) {
-                    pert_array.push({
-                        'id': '' + j[i].TaskID + '',
-                        'duration': j[i].Number_of_days,
-                        'name': '' + j[i].TaskID + '',
-                        'fullName': j[i].Name,
-                    })
+        google.charts.load('current', {'packages':['gantt']});
+        google.charts.setOnLoadCallback(drawChart);
 
-                } else {
-                    var array = ["" + j[i].PredecessorTaskID + ""];
+        function daysToMilliseconds(days) {
+        return days * 24 * 60 * 60 * 1000;
+        }
 
-                    pert_array.push({
-                        'id': '' + j[i].TaskID + '',
-                        'duration': j[i].Number_of_days,
-                        'name': '' + j[i].TaskID + '',
-                        'dependsOn': array,
-                        'fullName': j[i].Name,
-                    })
+        function drawChart() {
 
-                }
+        var data = new google.visualization.DataTable();
+        var start, end;
+        data.addColumn('string', 'Task ID');
+        data.addColumn('string', 'Task Name');
+        data.addColumn('date', 'Start Date');
+        data.addColumn('date', 'End Date');
+        data.addColumn('number', 'Duration');
+        data.addColumn('number', 'Percent Complete');
+        data.addColumn('string', 'Dependencies');
+    
+
+        for (var i = 0; i < j.length; i++) {
+
+            if(j[i].Start_Date === null){
+                start = null;
+            }else{
+                start = new Date(moment(j[i].Start_Date));
+                // start = null;
+            }
+            
+            if(j[i].End_Date === null){
+                end = null;
+            }else{
+                end = new Date(moment(j[i].End_Date));
+                // end = null;
+            }
+
+            if (j[i].PredecessorTaskID == 0) {
+
+                array.push([
+                    
+                    j[i].Name,
+                    String(j[i].TaskID),
+                    start,
+                    end,
+                    daysToMilliseconds(j[i].Number_of_days),
+                    j[i].Percentage,
+                    null
+                ]);
+
+            } else {
+                array.push([
+                   
+                    j[i].Name,
+                    String(j[i].TaskID),
+                    start,
+                    end,
+                    daysToMilliseconds(j[i].Number_of_days),
+                    j[i].Percentage,
+                    String(j[i].PredecessorTaskID)
+                ]);
 
             }
 
-            // data
-            data = pert_array;
+        }
 
-            // set filling method for tree
-            var treeData = anychart.data.tree(data, 'as-table');
+      data.addRows(array);
 
-            // create PERT chart
-            var chart = anychart.pert();
-            // set spacing between milestones
-            chart.data(treeData)
-                .horizontalSpacing('20%')
-                // set padding for chart
-                .padding([25, 50, 0, 50]);
+      var options = {
+        height: 275,
+        gantt:{
+            defaultStartDateMills: new Date(2019,3,28)
+        }
+      };
 
-            // get duration project
-            var duration = chart.getStat('pertChartProjectDuration');
+      var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
 
-            // set title text
-            chart.title()
-                .enabled(true)
-                .useHtml(true)
-                .padding([0, 0, 35, 0])
-                .text(
-                    "PERT Diagram For " + result.Name +
-                    "<br>" + "Project duration: " +
-                    duration + ' days'
-                );
+      chart.draw(data, options);
+    }
 
-            // set settings for tasks
-            var tasks = chart.tasks();
-            // format upper label task
-            tasks.upperLabels().format(function () {
-                return this.item.get('fullName');
-            });
-
-            // format lower label task
-            tasks.lowerLabels().format('{%duration} days');
-
-            // create tasks tooltip
-            var taskTooltip = tasks.tooltip();
-            // tooltip settings
-            taskTooltip.separator(true)
-                .titleFormat(function () {
-                    // return fullName from data
-                    return this.item.get('fullName');
-                });
-            taskTooltip.title().useHtml(true);
-
-            // set settings for milestones
-            var milestones = chart.milestones();
-            // set milestones color
-            milestones.color('#2C81D5')
-                // set milestones item size
-                .size('6.5%');
-            milestones.hovered().fill(function () {
-                return anychart.color.lighten(this.sourceColor, 0.25);
-            });
-            milestones.tooltip().format(defuaultMilesoneTooltipTextFormatter);
-
-            // set settings for critical milestones
-            var critMilestones = chart.criticalPath().milestones();
-            // format label
-            critMilestones.labels().format(function () {
-                return this['creator'] ? this['creator'].get('name') : this['isStart'] ? 'Start' : 'Finish';
-            });
-            // set color
-            critMilestones.color('#E24B26')
-                // fill color for critMilestones item
-                .fill(function () {
-                    return this['creator'] ? this.sourceColor : this['isStart'] ? '#60727B' : '#60727B';
-                });
-
-            // hover fill/stroke color for critMilestones item
-            critMilestones.hovered()
-                .fill(function () {
-                    return this['creator'] ? anychart.color.lighten(this.sourceColor, 0.25) : this['isStart'] ? '#60727b' : '#60727b';
-                })
-                .stroke(function () {
-                    return this['creator'] ? '1.5 #a94e3d' : null;
-                });
-
-            // set container id for the chart
-            chart.container('chart');
-            // initiate chart drawing
-            chart.draw();
-
-            //Remove loading icon and display output
-            document.getElementById("load").style.display = "none";
-            document.getElementById("container_content").classList.remove("display-none");
+    //Addnames on sidebar and breadcrumb
+    document.getElementById("project_name_side").innerHTML = result.Name;
+    document.getElementById("project_link").href = "view_project.html?"+sessionStorage.getItem('ProjectID');
+    document.getElementById("project_name_crumb").innerHTML = result.Name;
 
 
-            //Remove loading icon and display output for sidebar
-            document.getElementById("icon_container").classList.remove("display-none");
-            document.getElementById("sidebarToggle").classList.remove("display-none");
+    //Remove loading icon and display output
+    document.getElementById("load").style.display = "none";
+    document.getElementById("container_content").classList.remove("display-none");
 
-        }).catch(error => console.error('Error:', error));
+
+    //Remove loading icon and display output for sidebar
+    document.getElementById("icon_container").classList.remove("display-none");
+    document.getElementById("sidebarToggle").classList.remove("display-none");
+
+}).catch(error => console.error('Error:', error));
 });
-
-function defuaultMilesoneTooltipTextFormatter() {
-    var result = '';
-    var i = 0;
-    if (this['successors'] && this['successors'].length) {
-        result += 'Successors:';
-        for (i = 0; i < this['successors'].length; i++) {
-            result += '\n - ' + this['successors'][i].get('fullName');
-        }
-        if (this['predecessors'] && this['predecessors'].length)
-            result += '\n\n';
-    }
-    if (this['predecessors'] && this['predecessors'].length) {
-        result += 'Predecessors:';
-        for (i = 0; i < this['predecessors'].length; i++) {
-            result += '\n - ' + this['predecessors'][i].get('fullName');
-        }
-    }
-    return result;
-}
